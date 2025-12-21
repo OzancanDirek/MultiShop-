8c940859f46613dcc20233e7c31a21fa7b002dcf
Ozancan Direk <ozan_direk@hotmail.com>
2025-12-21 17:35:40 +0300

    ietisim sayfasi icin arayüz tasarlandi ve sepet islemleri ayarlandi

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ShoppingCardViewComponent/_ShoppingCardProductListComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ShoppingCardViewComponent
+{
+    public class _ShoppingCardProductListComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
