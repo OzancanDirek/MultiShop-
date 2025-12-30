cd67c3213d87ec7e1f77eb67a00657495e291c2e
Ozancan Direk <ozan_direk@hotmail.com>
2025-12-31 01:00:01 +0300

    feature slider kismi dinamik hale getirildi,arayuz tasarlandi

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponent/_CarouselDefaultComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponent/_CarouselDefaultComponentPartial.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.FeatureSliderDto;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViiewComponent
 {
     public class _CarouselDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CarouselDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7071/api/FeatureSlider");
+            var statusCode = responseMessage.StatusCode;
+            var content = await responseMessage.Content.ReadAsStringAsync();
+            ViewBag.ApiStatus = $"Status: {statusCode}";
+            ViewBag.ApiResponse = content;
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureSliderDto>>(content);
+                return View(values ?? new List<ResultFeatureSliderDto>());
+            }
+            return View(new List<ResultFeatureSliderDto>());
         }
     }
 }
---
+++ Services/Catalog/MultiShop.Catalog/Controllers/FeatureSliderController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.FeatureSliderDtos;
+using MultiShop.Catalog.Services.FeatureSliderServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeatureSliderController : ControllerBase
+    {
+        private readonly IFeatureSliderService _featureSliderService;
+
+        public FeatureSliderController(IFeatureSliderService featureSliderService)
+        {
+            _featureSliderService = featureSliderService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> FeatureSliderList()
+        {
+            var values = await _featureSliderService.GetAllFeatureAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFeatureSliderById(string id)
+        {
+            var values = await _featureSliderService.GetByIdFeatureAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFeatureSlider(CreateFeatureSliderDto createFeatureSliderDto)
+        {
+            await _featureSliderService.CreateFeatureAsync(createFeatureSliderDto);
+            return Ok("Öne çıkan görsel basariyla eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteFeatureSlider(string id)
+        {
+            await _featureSliderService.DeleteFeatureAsync(id);
+            return Ok("Öne çıkan görsel basariyla silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateFeatureSlider(UpdateFeatureSliderDto updateFeatureSliderDto)
+        {
+            await _featureSliderService.UpdateFeatureAsync(updateFeatureSliderDto);
+            return Ok("Öne çıkan görsel basariyla guncellendi");
+        }
+    }
+}
