208877612d0d659e9b597d291196977624770311
Ozancan Direk <ozan_direk@hotmail.com>
2026-01-20 19:06:04 +0300

    Contact sayfasi tasarlandi ve yetkiliye bu sayfadan mesaj gonderilebilir

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ContactController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ContactDtos;
+using MultiShop.Catalog.Services.ContactServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactService _ContactService;
+
+        public ContactController(IContactService ContactService)
+        {
+            _ContactService = ContactService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ContactList()
+        {
+            var values = await _ContactService.GetAllContactAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetContactById(string id)
+        {
+            var values = await _ContactService.GetByIdContactAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateContact(CreateContactDto createContactDto)
+        {
+            await _ContactService.CreateContactAsync(createContactDto);
+            return Ok("Mesaj basariyla eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteContact(string id)
+        {
+            await _ContactService.DeleteContactAsync(id);
+            return Ok("Mesaj basariyla silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateContact(UpdateContactDto updateContactDto)
+        {
+            await _ContactService.UpdateContactAsync(updateContactDto);
+            return Ok("Mesaj basariyla guncellendi");
+        }
+    }
+}
