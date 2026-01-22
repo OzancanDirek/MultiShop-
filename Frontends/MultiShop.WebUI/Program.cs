30e6729a2075c4bfe0f5d6c60d812536cfe2bccf
Ozancan Direk <ozan_direk@hotmail.com>
2026-01-22 15:51:15 +0300

    Token islemleri icin generateToken olusturuldu ve basariyla zincirler tamamlandi

--- Frontends/MultiShop.WebUI/Program.cs
+++ Frontends/MultiShop.WebUI/Program.cs
@@ -1,8 +1,26 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MultiShop.WebUI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddCookie(JwtBearerDefaults.AuthenticationScheme, opt =>
+{
+    opt.LoginPath = "/Login/Index";
+    opt.LogoutPath = "/Login/Logout";
+    opt.AccessDeniedPath = "/Pages/AccessDenied";
+    opt.Cookie.HttpOnly = true;
+    opt.Cookie.SameSite = SameSiteMode.Strict;
+    opt.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    opt.Cookie.Name = "MultiShopJwt";
+});
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddScoped<ILoginService, LoginService>();
 
 builder.Services.AddHttpClient();
 builder.Services.AddControllersWithViews();
-builder.Logging.AddConsole();//skntý
+builder.Logging.AddConsole();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
@@ -25,6 +43,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();
 
 app.UseCors("AllowAll");
 
---
+++ MultiShop/IdentityServer/MultiShop.IdentityServer/Tools/JwtTokenGenerator.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MultiShop.IdentityServer.Tools
+{
+    public class JwtTokenGenerator
+    {
+        public static TokenResponseViewModel GenerateToken(GetCheckAppUserViewModel model)
+        {
+            var claims = new List<Claim>();
+
+            if (!string.IsNullOrEmpty(model.Role))
+                claims.Add(new Claim(ClaimTypes.Role, model.Role));
+
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, model.Id));
+
+            if (!string.IsNullOrEmpty(model.Username))
+                claims.Add(new Claim("Username", model.Username));
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(JwtTokenDefaults.Key)
+            );
+
+            var signingCredentials =
+                new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            var expireDate = DateTime.UtcNow.AddDays(JwtTokenDefaults.Expire);
+
+            var token = new JwtSecurityToken(
+                issuer: JwtTokenDefaults.ValidIssuer,
+                audience: JwtTokenDefaults.ValidAudience,
+                claims: claims,
+                notBefore: DateTime.UtcNow,
+                expires: expireDate,
+                signingCredentials: signingCredentials
+            );
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return new TokenResponseViewModel(tokenHandler.WriteToken(token), expireDate);
+        }
+    }
+}
