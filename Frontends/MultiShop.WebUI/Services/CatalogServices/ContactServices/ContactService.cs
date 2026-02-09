6b3dcc3d681d46f9b48d4aa0a5a6c2acb9bb4f7a
Ozancan Direk <ozan_direk@hotmail.com>
2026-02-09 14:53:59 +0300

    Comment Microservice icin Ocelot ayarlamalari yapildi

---
+++ Frontends/MultiShop.WebUI/Services/CatalogServices/ContactServices/ContactService.cs
@@ -0,0 +1,44 @@
+﻿using MultiShop.DtoLayer.CatalogDtos.ContactDtos;
+using Newtonsoft.Json;
+
+namespace MultiShop.WebUI.Services.CatalogServices.ContactServices
+{
+    public class ContactService : IContactService
+    {
+        private readonly HttpClient _httpClient;
+        public ContactService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task CreateContactAsync(CreateContactDto createContactDto)
+        {
+            await _httpClient.PostAsJsonAsync<CreateContactDto>("Contact", createContactDto);
+        }
+
+        public async Task DeleteContactAsync(string id)
+        {
+            await _httpClient.DeleteAsync("Contact?id=" + id);
+        }
+
+        public async Task<List<ResultContactDto>> GetAllContactAsync()
+        {
+            var responseMessage = await _httpClient.GetAsync("Contact");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+            return values;
+        }
+
+        public async Task<GetByIdContactDto> GetByIdContactAsync(string ContactId)
+        {
+            var responseMessage = await _httpClient.GetAsync("Contact/" + ContactId);
+            var values = await responseMessage.Content.ReadFromJsonAsync<GetByIdContactDto>();
+            return values;
+        }
+
+        public async Task UpdateContactAsync(UpdateContactDto updateContactDto)
+        {
+            await _httpClient.PutAsJsonAsync<UpdateContactDto>("Contact", updateContactDto);
+        }
+    }
+}
