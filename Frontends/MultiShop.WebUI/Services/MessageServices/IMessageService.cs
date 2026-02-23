dda729f1e9f45a0dd502f068a08ece1be7974e7c
Ozancan Direk <ozan_direk@hotmail.com>
2026-02-23 22:54:59 +0300

    Mesajlar icin microservice kuruldu ve UI tasarimi yapildi

---
+++ Frontends/MultiShop.WebUI/Services/MessageServices/IMessageService.cs
@@ -0,0 +1,10 @@
+﻿using MultiShop.DtoLayer.MessageDtos;
+
+namespace MultiShop.WebUI.Services.MessageServices
+{
+    public interface IMessageService
+    {
+        Task<List<ResultInboxMessageDto>> GetInboxMessageAsync(string id);
+        Task<List<ResultSendBoxMessageDto>> GetSendMessageAsync(string id);
+    }
+}
---
+++ Services/Message/MultiShop.Message/Mapping/GeneralMapping.cs
@@ -0,0 +1,18 @@
+﻿using MultiShop.Message.DAL.Entities;
+using MultiShop.Message.Dtos;
+
+namespace MultiShop.Message.Mapping
+{
+    public class GeneralMapping : AutoMapper.Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<UserMessage,ResultMessageDto>().ReverseMap();
+            CreateMap<UserMessage,CreateMessageDto>().ReverseMap();
+            CreateMap<UserMessage,UpdateMessageDto>().ReverseMap();
+            CreateMap<UserMessage,ResultInboxMessageDto>().ReverseMap();
+            CreateMap<UserMessage,ResultSendBoxDto>().ReverseMap();
+            CreateMap<UserMessage,GetByIdMessageDto>().ReverseMap();
+        }
+    }
+}
